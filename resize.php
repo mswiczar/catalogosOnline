1e154c2c40f912e81b17cb54721685f99895426c
Moshe <60067147+mswiczar@users.noreply.github.com>
2020-04-15 19:20:31 -0300

    Add files via upload

---
+++ resize.php
@@ -0,0 +1,16 @@
+<?
+        include_once "dbconnections.php";
+        include_once "simpleImage.php";
+        dbconnect ();
+		$sql= "select * from marca order by nombre";
+		$result = mysql_query ($sql,$GLOBALS['db_cap']);                                                                   
+		while ( $r =    mysql_fetch_assoc($result))
+       {
+			$imageFUll= "img/".$r['PORTADA'];
+			$target_file = "img/min-".$r['PORTADA'];
+			$image = new SimpleImage();
+		    $image->load($imageFUll);
+    		$image->scale(10);
+		    $image->save($target_file);	
+	   }
+?>
