77b7abb18f3cfe1662b626bf88336b26b2db5411
Moshe <60067147+mswiczar@users.noreply.github.com>
2020-04-15 19:21:11 -0300

    Update resize.php

--- resize.php
+++ resize.php
@@ -5,12 +5,12 @@
 		$sql= "select * from marca order by nombre";
 		$result = mysql_query ($sql,$GLOBALS['db_cap']);                                                                   
 		while ( $r =    mysql_fetch_assoc($result))
-       {
+       		{
 			$imageFUll= "img/".$r['PORTADA'];
 			$target_file = "img/min-".$r['PORTADA'];
 			$image = new SimpleImage();
-		    $image->load($imageFUll);
-    		$image->scale(10);
-		    $image->save($target_file);	
-	   }
+			$image->load($imageFUll);
+    			$image->scale(10);
+		    	$image->save($target_file);	
+		}
 ?>
